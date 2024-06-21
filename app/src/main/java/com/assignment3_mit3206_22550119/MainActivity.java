debaac301efcedcfd7a8d8ece1be985f22dc98c3
NavinduMadusanka <navindumadusankadias@gmail.com>
2024-06-21 22:21:45 +0530

    Initial Commit

---
+++ app/src/main/java/com/assignment3_mit3206_22550119/MainActivity.java
@@ -0,0 +1,14 @@
+package com.assignment3_mit3206_22550119;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
